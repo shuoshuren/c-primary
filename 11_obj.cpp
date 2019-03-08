588d989575e246966f0225d84fe6142c1163e400
shuoshuren <534361124@qq.com>
2019-03-08 23:04:26 +0800

    成员函数定义

--- 11_obj.cpp
+++ 11_obj.cpp
@@ -11,27 +11,41 @@ using namespace std;
  * 
  * 
  */
-
-
 class Student
 {
 private:
     int num;//学号
     char name[100];//姓名
+    bool sex;//性别
     int arg;//年龄
 protected:
     int score;//分数
 public:
     int grade;//年级
 
-    //学习的行为
+    //学习，在类的内部定义实现，属于内联函数
     void learn(){
+
+        cout<<"上课"<<endl;
     }
 
-    //考试
-    void exam(){
+    void doExercise();
+    //做家庭作业
+    void doHomework();
+};
 
-    }
+
+/**
+ * 做锻炼
+ */ 
+void Student::doExercise(){
+    cout<<"做锻炼"<<endl;
+}
+/**
+ * //做家庭作业
+ */   
+void Student::doHomework(){
+    cout<<"做家庭作业"<<endl;
 }
 
 
@@ -39,7 +53,12 @@ int main(){
 
     //定义学生的变量，对象
     Student stu;
+    stu.grade = 6;
+    //用“.”访问类的共有成员变量和函数
+    cout<<stu.grade<<endl;
 
+    stu.doHomework();
+    stu.doExercise();
 
     return 0;
 }
